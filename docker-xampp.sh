509fabc601db5fe3357258ac2490e565634fcd72
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-07-10 16:34:28 +0200

    "feat: Modificar docker-xampp.sh
    Añadir carpeta etc y cambiar propietario de las carpetas pages y etc"

--- docker-xampp.sh
+++ docker-xampp.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
 mkdir -p pages
+mkdir -p etc
 
 echo '<html><head><meta charset="UTF-8"><title>Documento</title></head><body><h1>Hola mundo</h1></body></html>' > pages/index.html
 
-docker run -dit \
+docker run -d \
     --name xampp \
     -v ${PWD}/pages:/opt/lampp/htdocs \
+    -v ${PWD}/etc:/opt/lampp/etc \
     -p 80:80 \
     -p 3306:3306 \
     xampp
+
+chown -R ${UID} pages etc
