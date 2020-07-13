9b7e0e74c020ca2df9ad5e115ed5c67ca322dc11
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-07-13 20:55:49 +0200

    "feat: Añadir fichero sql
    Añadir fichero sql con base de datos y tabla para el proyecto,
    la tabla contiene personas y se ha comentado el insert de tres personas
    de prueba"

---
+++ sql/skills.sql
@@ -0,0 +1,21 @@
+create database skills;
+
+use skills;
+
+create table personas (num_persona int auto_increment primary key,
+                       dni varchar(9),
+                       nombre varchar(30) not null,
+                       apellido_1 varchar(30) not null,
+                       apellido_2 varchar(30),
+                       fecha_de_nac date,
+                       genero character check (genero = "H" or genero = "F"),
+                       vivo boolean default true,
+                       descripcion text);
+
+/*Datos de prueba
+
+insert into personas (dni, nombre, apellido_1, apellido_2, fecha_de_nac, genero, vivo, descripcion) values
+    ("12345678K", "Manolo", "Ruíz", "Saenz", "1989-4-12", "H", true, "Hombre mayor de poca estatura y un tatuake en el cuello"),
+    ("65789534F", "Francisco", "Torres", "Puigdemont", "1980-1-23", "H", true, "Hombre mayor, le gusta que le llamen Paco"),
+    ("12348765G", "María", "Pérez", "Martínez", "1995-10-4", "F", true, "Mujer joven, vive con sus padres ancianos para cuidarlos");
+*/
