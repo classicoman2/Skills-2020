79e823c4f41f92a0d32212a891aaaebfd50b1c53
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-07-19 20:39:02 +0200

    "feat: Devolver json con personas
    Devolver json con personas en vez de html"

--- pages/crud/personas.php
+++ pages/crud/personas.php
@@ -1,97 +1,37 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Personas</title>
-</head>
-<body>
-    <?php
-        $servername = "192.168.1.20";
-        $username = "skills";
-        $password = "1234";
-        $database = "skills";
-        
-        // Crear conexión
-        $conn = new mysqli($servername, $username, $password, $database);
-        
-        // Comprobar conexión
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        } else {
-            echo "<h1>Correctamente conectado a la base de datos</h1>";
+<?php
+
+    header("Content-Type: application/json");
+    $json = new stdClass();
+
+    $conexion = new mysqli("192.168.1.20", "skills", "1234", "skills");
+
+    if ($conexion->connect_error) { $json->conexion = "failed"; }
+    else { $json->conexion = "succes"; }
+
+    $query = "select dni, nombre, apellido_1, apellido_2, fecha_de_nac,
+    genero, vivo, descripcion from personas order by apellido_1 asc";
+
+    $resultadoQuery = $conexion->query($query);
+
+    $json->personas = array();
+
+    if ($resultadoQuery->num_rows > 0) {
+        while ($fila = $resultadoQuery->fetch_assoc()) {
+            $persona = new stdClass();
+            $persona->dni = $fila["dni"];
+            $persona->nombre = $fila["nombre"];
+            $persona->apellido_1 = $fila["apellido_1"];
+            $persona->apellido_2 = $fila["apellido_2"];
+            $persona->fecha_de_nac = $fila["fecha_de_nac"];
+            $persona->genero = $fila["genero"];
+            $persona->vivo = $fila["vivo"];
+            $persona->descripcion = $fila["descripcion"];
+            $json->personas[count($json->personas)] = $persona;
         }
+    }
 
-        // Sentencia SQL
-        $sql = "select dni, nombre, apellido_1, apellido_2, fecha_de_nac,
-        genero, vivo, descripcion from personas order by apellido_1 asc";
+    $conexion->close();
 
-        $resultadoQuery = $conn->query($sql);
-    ?>
+    echo json_encode($json);
 
-        <table>
-            <thead>
-                <tr>
-                    <th>DNI</th>
-                    <th>Nombre</th>
-                    <th>Primer apellido</th>
-                    <th>Segundo apellido</th>
-                    <th>Fecha de nacimiento</th>
-                    <th>Genero</th>
-                    <th>Vivo</th>
-                    <th>Descripción</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    if ($resultadoQuery->num_rows > 0) {
-                        // Rellenar la tabla
-                        while ($fila = $resultadoQuery->fetch_assoc()) {
-                            echo "<tr>";
-                                echo "<td>";
-                                    echo $fila["dni"];
-                                echo "</td>";
-                                echo "<td>";
-                                    echo $fila["nombre"];
-                                echo "</td>";
-                                echo "<td>";
-                                    echo $fila["apellido_1"];
-                                echo "</td>";
-                                echo "<td>";
-                                    echo $fila["apellido_2"];
-                                echo "</td>";
-                                echo "<td>";
-                                    echo $fila["fecha_de_nac"];
-                                echo "</td>";
-                                echo "<td>";
-                                    if ($fila["genero"] == "H") {
-                                        echo "Hombre: " . $fila["genero"];
-                                    } else {
-                                        echo "Mujer: " . $fila["genero"];
-                                    }
-                                    
-                                echo "</td>";
-                                echo "<td>";
-                                    if ($fila["vivo"] == 1) {
-                                        echo "Vivo";
-                                    } else {
-                                        echo "Muerto";
-                                    }
-                                echo "</td>";
-                                echo "<td>";
-                                    echo $fila["descripcion"];
-                                echo "</td>";
-                            echo "</tr>";
-                        }
-                    } else {
-                        echo "<tr>Sin resultados</tr>";
-                    }
-                ?>
-            </tbody>
-        </table>
-
-    <?php
-    //Cerrar conexióm
-        $conn->close();
-    ?>
-</body>
-</html>
+?>
