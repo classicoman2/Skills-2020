b6bf4b15495270a35e67344ec795447878d39baf
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-07-20 22:06:46 +0200

    feat: Añadir página php que borra personas
    Añadir php que borrar personas según su dni con el método http delete

---
+++ pages/crud/delete.php
@@ -0,0 +1,16 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+        if (!isset($_REQUEST["dni"])) die();
+    } else {
+        die("El método de la petición debe ser DELETE");
+    }
+
+    $conexion = new mysqli("192.168.1.20", "skills", "1234", "skills");
+    if ($conexion->connect_error) die("Ha habido un error en la conexión");
+
+    $query = "delete from personas where dni = '" . $_REQUEST["dni"] . "'";
+
+    if ($conexion->query($query) !== TRUE) die("No se ha borrado el registro");
+
+    echo "Borrado";
+?>
