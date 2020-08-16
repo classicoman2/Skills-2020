256a26a2b618ec0d36e0dda082f26f7c6b3bdad9
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-08-16 22:06:57 +0200

    feat: Añadir javascript
    Añadir codigo javascript que hace llamadas ajax al servidor
    y devuelve objetos json con personas

---
+++ pages/scripts/index.js
@@ -0,0 +1,58 @@
+let cuerpoTabla = document.querySelector("table.campos > tbody");
+
+function vaciarTabla() {
+    let filasTabla = document.querySelectorAll("table.campos > tbody tr");
+    for (let fila of filasTabla) {
+        if (fila.id != "filaFormulario") {
+            fila.remove();
+        }
+    }
+}
+
+function llenarTabla(json) {
+    vaciarTabla();
+    for (let persona of json.personas) {
+        let fila = document.createElement("tr");
+        fila.innerHTML = "<td>" + persona.dni
+                        + "</td><td>" + persona.nombre
+                        + "</td><td>" + persona.apellido_1
+                        + "</td><td>" + persona.apellido_2
+                        + "</td><td>" + persona.fecha_de_nac
+                        + "</td><td>" + persona.genero
+                        + "</td><td>" + persona.vivo
+                        + "</td><td>" + persona.descripcion
+                        + "</td><td><button><img src='img/editar.png' alt='editar' height='100%' width='100%'></button></td>";
+        cuerpoTabla.innerHTML = fila.innerHTML + cuerpoTabla.innerHTML;
+    }
+}
+
+function getPersonas() {
+    fetch("/crud/personas", {method: "GET"})
+        .then(respuesta => respuesta.json())
+        .then(json => llenarTabla(json));
+}
+
+function ponerPersonaEnTabla(persona) {
+    vaciarTabla();
+    let fila = document.createElement("tr");
+    fila.innerHTML = "<td>" + persona.dni
+                    + "</td><td>" + persona.nombre
+                    + "</td><td>" + persona.apellido_1
+                    + "</td><td>" + persona.apellido_2
+                    + "</td><td>" + persona.fecha_de_nac
+                    + "</td><td>" + persona.genero
+                    + "</td><td>" + persona.vivo
+                    + "</td><td>" + persona.descripcion
+                    + "</td><td><button><img src='img/editar.png' alt='editar' height='100%' width='100%'></button></td>";
+    cuerpoTabla.innerHTML = fila.innerHTML + cuerpoTabla.innerHTML;
+}
+
+function buscarPersona(formulario) {
+    let inputDni = formulario.querySelector("input[name=dni]");
+    fetch("/crud/persona?dni=" + inputDni.value, {method: "GET"})
+        .then(respuesta => respuesta.json())
+        .then(json => ponerPersonaEnTabla(json));
+    inputDni.value = "";
+}
+
+getPersonas();
