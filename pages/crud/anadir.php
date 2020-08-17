a8bca47a92990568f9d14518d76cb0e26e7eb54b
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-08-17 20:50:50 +0200

    feat: Añadir codigo en anadir.php
    Añadir codigo en anadir.php que añade una persona recibida por
    un json a la base de datos

--- pages/crud/anadir.php
+++ pages/crud/anadir.php
@@ -1,49 +1,20 @@
-<style>
-p {
-    font-weight: bold;
-    font-size: large;
-}
-</style>
 <?php
-    echo "<h1>Se añadirá la siguiente persona:</h1>";
-    
-    echo "<p>DNI:</p>";
-    echo $_POST["dniPersona"];
-    echo "<p>Nombre:</p>";
-    echo $_POST["nombrePersona"];
-    echo "<p>Primer apellido:</p>";
-    echo $_POST["apellidoUnoPersona"];
-    echo "<p>Segundo apellido:</p>";
-    echo $_POST["apellidoDosPersona"];
-    echo "<p>Fecha de nacimiento:</p>";
-    echo $_POST["fechaPersona"];
-    echo "<p>Género:</p>";
-    if ($_POST["generoPersona"] == "H") {
-        echo "Hombre " . $_POST["generoPersona"];
-    } else {
-        echo "Mujer " . $_POST["generoPersona"];
-    }
-    echo "<p>Vivo:</p>";
-    if ($_POST["vivoPersona"] == "true") {
-        echo "Vivo " . $_POST["vivoPersona"];
-    } else {
-        echo "Muerto " . $_POST["vivoPersona"];
-    }
-    echo "<p>Descripción::</p>";
-    echo $_POST["descripcionPersona"];
 
-    $conexion = new mysqli("192.168.1.20", "skills", "1234", "skills");
-    if ($conexion->connect_error) { die("Ha habido un error en la conexión"); }
+$json = file_get_contents('php://input');
 
-    $query = "insert into personas (dni, nombre, apellido_1, apellido_2, fecha_de_nac, genero, vivo, descripcion)
-    values ('" . $_POST["dniPersona"] . "','" . $_POST["nombrePersona"] . "','" . $_POST["apellidoUnoPersona"] .
-    "','" . $_POST["apellidoDosPersona"] . "','" . $_POST["fechaPersona"] . "','" . $_POST["generoPersona"] .
-    "', " . $_POST["vivoPersona"] . ",'" . $_POST["descripcionPersona"] . "');";
+$datos = json_decode($json);
 
-    if ($conexion->query($query) === TRUE) {echo "<p>añadido</p>";}
-    else {echo "<p>Error</p>";}
-    $conexion->close();
+$query = "insert into personas (dni, nombre, apellido_1, apellido_2, fecha_de_nac, genero, vivo, descripcion)
+values ('$datos->dni', '$datos->nombre', '$datos->papellido', '$datos->sapellido', '$datos->fechanac', '$datos->genero',
+$datos->vivo, '$datos->descripcion')";
+
+header("Content-Type: application/json");
+
+$conexion = new mysqli("192.168.1.20", "skills", "1234", "skills");
+if ($conexion->connect_error) { die("{\"anadido\": false}"); }
+
+if ($conexion->query($query) === TRUE) {echo "{\"anadido\": true}";}
+else {echo "{\"anadido\": false}";}
+$conexion->close();
+
 ?>
-
-<br/>
-<a href="../index.html">Volver al formulario</a>
