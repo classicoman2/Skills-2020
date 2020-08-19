b3a60227d13a5c294e3be330947c2cb68f07ee9d
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-08-19 20:52:53 +0200

    fix: Añadir condicion para que vivo no este vacio
    Añadir condicion que evalua que el campo "vivo" no esta
    vacio ya que false en php es un cadena vacia, se han
    sustituido los valores booleanos por numericos

--- pages/crud/anadir.php
+++ pages/crud/anadir.php
@@ -4,6 +4,9 @@ $json = file_get_contents('php://input');
 
 $datos = json_decode($json);
 
+if ($datos->vivo == "on") { $datos->vivo = 1; }
+else { $datos->vivo = 0; }
+
 $query = "insert into personas (dni, nombre, apellido_1, apellido_2, fecha_de_nac, genero, vivo, descripcion)
 values ('$datos->dni', '$datos->nombre', '$datos->papellido', '$datos->sapellido', '$datos->fechanac', '$datos->genero',
 $datos->vivo, '$datos->descripcion')";
@@ -13,8 +16,8 @@ header("Content-Type: application/json");
 $conexion = new mysqli("192.168.1.20", "skills", "1234", "skills");
 if ($conexion->connect_error) { die("{\"anadido\": false}"); }
 
-if ($conexion->query($query) === TRUE) {echo "{\"anadido\": true}";}
-else {echo "{\"anadido\": false}";}
+if ($conexion->query($query) === TRUE) { echo "{\"anadido\": true}"; }
+else { echo "{\"anadido\": false}"; }
 $conexion->close();
 
 ?>
