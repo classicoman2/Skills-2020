a3859a7acdb5c5930ffe08dccc992eab08fcfb23
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-08-21 20:41:43 +0200

    feat: Añadir codigo que actualiza personas
    Añadir codigo PHP que actualiza personas recibidas en un json
    con el metodo PUT

--- pages/crud/update.php
+++ pages/crud/update.php
@@ -1,5 +1,24 @@
 <?php
 
-//TODO: hacer esto
+header("Content-Type: application/json");
+
+if ($_SERVER["REQUEST_METHOD"] != "PUT") {
+    die("{\"actualizado\": false}");
+}
+
+$json = file_get_contents('php://input');
+$datos = json_decode($json);
+
+$query = "update personas set nombre = '$datos->nombre', apellido_1 = '$datos->papellido', apellido_2 = '$datos->sapellido',
+fecha_de_nac = '$datos->fechanac', genero = '$datos->genero', vivo = $datos->vivo, descripcion = '$datos->descripcion'
+where dni = '$datos->dni'";
+
+$conexion = new mysqli("192.168.1.20", "skills", "1234", "skills");
+if ($conexion->connect_error) { die("{\"actualizado\": false}"); }
+
+if ($conexion->query($query)) {echo "{\"actualizado\": true}";}
+else { die("{\"actualizado\": false}"); }
+
+$conexion->close();
 
 ?>
