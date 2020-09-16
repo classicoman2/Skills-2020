3db28b509cfc4795551a89918abb9f874eca70dc
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-09-16 19:49:12 +0200

    feat: Añadir clase BDConector
    Añadir clase BDConector que crear una conexion a la base
    de datos y ejecuta queries

---
+++ pages/crud/BDConector.php
@@ -0,0 +1,41 @@
+<?php
+
+class BDConector {
+
+    private $conexion;
+    private $direccion = "192.168.1.20";
+    private $bd = "skills";
+    private $usuario = "skills";
+    private $contrasena = "1234";
+
+    public function abrirConexion() {
+        $this->conexion = new mysqli(
+            $this->direccion,
+            $this->usuario,
+            $this->contrasena,
+            $this->bd);
+
+        if ($this->conexion->connect_error) { return false; }
+        else { return true; }
+    }
+
+    public function cerrarConexion() {
+        if (!$this->conexion) { return false; }
+
+        $this->conexion->close();
+        return true;
+    }
+
+    public function ejecutarQuery($query) {
+        if (!$this->conexion) { return false; }
+
+        $resultado = $this->conexion->query($query);
+
+        if ($resultado->num_rows == 0) { return false; }
+
+        return $resultado;
+    }
+
+}
+
+?>
